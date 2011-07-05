9f298366a92700cb658e26c16b34e5a8ee0c21bd
Antoine Bertin <diaoulael@gmail.com>
2011-07-05 02:27:11 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+subliminal
+==========
+Python module to search and download subtitles
+
+* [Project page](https://github.com/Diaoul/subliminal)
+* [Initial project by Patrick Dessalle](http://code.google.com/p/periscope/)
---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Subliminal - Subtitles, faster than your thoughts
+# Copyright (c) 2011 Antoine Bertin <diaoulael@gmail.com>
+#
+# Subliminal is free software; you can redistribute it and/or modify it under
+# the terms of the Lesser GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Subliminal is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# Lesser GNU General Public License for more details.
+#
+# You should have received a copy of the Lesser GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+from setuptools import setup
+from subliminal import __version__
+
+
+setup(name='subliminal',
+    version=__version__,
+    license="LGPLv3",
+    description="Subliminal - Subtitles, faster than your thoughts",
+    classifiers=['Development Status :: 4 - Beta',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.6',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Multimedia :: Video'],
+    keywords='subliminal video movie subtitle python library',
+    author="Antoine Bertin",
+    author_email="diaoulael@gmail.com",
+    url="https://github.com/Diaoul/subliminal",
+    packages=["subliminal", "subliminal/plugins"],
+    py_modules=["subliminal"],
+    install_requires=["BeautifulSoup>=3.2.0", "guessit>=0.2"])

fa25502803d6a5e5286d5dd0a5e59bee4d152b84
Antoine Bertin <diaoulael@gmail.com>
2011-10-25 08:09:08 +0200

    Remove broken plugins from the list

--- subliminal/core.py
+++ subliminal/core.py
@@ -58,7 +58,7 @@ LANGUAGES = set(['aa', 'ab', 'ae', 'af', 'ak', 'am', 'an', 'ar', 'as', 'av', 'ay
                  'ru', 'rw', 'sa', 'sc', 'sd', 'se', 'sg', 'si', 'sk', 'sl', 'sm', 'sn', 'so', 'sq', 'sr', 'ss', 'st',
                  'su', 'sv', 'sw', 'ta', 'te', 'tg', 'th', 'ti', 'tk', 'tl', 'tn', 'to', 'tr', 'ts', 'tt', 'tw', 'ty',
                  'ug', 'uk', 'ur', 'uz', 've', 'vi', 'vo', 'wa', 'wo', 'xh', 'yi', 'yo', 'za', 'zh', 'zu'])  # ISO 639-1
-PLUGINS = ['BierDopje', 'OpenSubtitles', 'SubsWiki', 'Subtitulos', 'TheSubDB']
+PLUGINS = ['OpenSubtitles']
 API_PLUGINS = filter(lambda p: getattr(plugins, p).api_based, PLUGINS)
 IDLE, RUNNING, PAUSED = range(3)
 LANGUAGE_INDEX, PLUGIN_INDEX, PLUGIN_CONFIDENCE, MATCHING_CONFIDENCE = range(4)

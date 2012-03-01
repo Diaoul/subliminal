ad1cbc83189a2925b3a4ceeb0b9bee8b82815c52
Antoine Bertin <diaoulael@gmail.com>
2012-03-01 17:43:18 +0100

    Add subswiki, subtitulos and thesubdb services

--- subliminal/core.py
+++ subliminal/core.py
@@ -30,7 +30,7 @@ __all__ = ['SERVICES', 'LANGUAGE_INDEX', 'SERVICE_INDEX', 'SERVICE_CONFIDENCE', 
            'create_list_tasks', 'create_download_tasks', 'consume_task', 'matching_confidence',
            'key_subtitles', 'group_by_video']
 logger = logging.getLogger(__name__)
-SERVICES = ['opensubtitles', 'bierdopje']
+SERVICES = ['opensubtitles', 'bierdopje', 'subswiki', 'subtitulos', 'thesubdb']
 LANGUAGE_INDEX, SERVICE_INDEX, SERVICE_CONFIDENCE, MATCHING_CONFIDENCE = range(4)
 
 

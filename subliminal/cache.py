728067d435317290027dc64003064d5b8b07c8b1
Antoine Bertin <diaoulael@gmail.com>
2012-12-15 13:33:25 +0100

    Use key_mangler to fix the error in dbm backend in dogpile.cache

--- subliminal/cache.py
+++ subliminal/cache.py
@@ -16,17 +16,6 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with subliminal.  If not, see <http://www.gnu.org/licenses/>.
 from dogpile.cache import make_region
-from dogpile.cache.util import function_key_generator
 
 
-# Fix for https://bitbucket.org/zzzeek/dogpile.cache/issue/12
-def my_key_generator(namespace, fn):
-    def generate_key(*args):
-        key = function_key_generator(namespace, fn)(*args)
-        if isinstance(key, unicode):
-            key = key.encode('utf-8')
-        print key
-        return key
-    return generate_key
-
-region = make_region(function_key_generator=my_key_generator)
+region = make_region(key_mangler=str)

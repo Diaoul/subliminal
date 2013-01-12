203dfd9709dd700f84d5988fe3092a1877bb806c
Nicolas Wack <wackou@gmail.com>
2013-01-12 22:47:44 +0100

    Compatibility fix for requests >= 1.0

--- subliminal/services/__init__.py
+++ subliminal/services/__init__.py
@@ -79,7 +79,15 @@ class ServiceBase(object):
     def init(self):
         """Initialize connection"""
         logger.debug(u'Initializing %s' % self.__class__.__name__)
-        self.session = requests.session(timeout=10, headers={'User-Agent': self.user_agent})
+        if requests.__version__[0] >= '1':
+            self.session = requests.Session()
+            # TODO: really necessary? can't we just remove it?
+            # note: if needed, it has to go in the .get() call, in requests>=1 timeout
+            #       doesn't exist anymore as a property of the session
+            #self.session.timeout = 10
+            self.session.headers = {'User-Agent': self.user_agent}
+        else:
+            self.session = requests.session(timeout=10, headers={'User-Agent': self.user_agent})
 
     def terminate(self):
         """Terminate connection"""

aab8e0aa4d8fb264f3f51bc340a606f95ceef0b4
Antoine Bertin <diaoulael@gmail.com>
2013-11-04 23:14:25 +0100

    Fix spacing around operator

--- subliminal/video.py
+++ subliminal/video.py
@@ -184,7 +184,7 @@ def scan_video(path, subtitles=True, embedded_subtitles=True):
     logger.info('Scanning video %r in %r', filename, dirpath)
     video = Video.fromguess(path, guessit.guess_file_info(filename, 'autodetect'))
     video.size = os.path.getsize(path)
-    if video.size  > 10485760:
+    if video.size > 10485760:
         logger.debug('Size is %d', video.size)
         video.hashes['opensubtitles'] = hash_opensubtitles(path)
         video.hashes['thesubdb'] = hash_thesubdb(path)

efe944a10c2afb0c0c9371c31262dd7d35e2c67d
Antoine Bertin <diaoulael@gmail.com>
2013-12-01 10:17:57 +0100

    Remove extra end of line character in cli

--- subliminal/cli.py
+++ subliminal/cli.py
@@ -158,7 +158,7 @@ def subliminal():
     # result output
     if not subtitles:
         if not args.quiet:
-            print('No subtitles downloaded\n', file=sys.stderr)
+            print('No subtitles downloaded', file=sys.stderr)
         exit(1)
     if not args.quiet:
         subtitles_count = sum([len(s) for s in subtitles.values()])

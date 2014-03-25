b2c6562c64cf98f88cafa96ee702f0cc9e96620a
northerndrifter <northerndrifter@gmail.com>
2014-03-25 21:01:13 +0100

    fixed hearing impaired flag for podnapisi

--- subliminal/providers/podnapisi.py
+++ subliminal/providers/podnapisi.py
@@ -118,14 +118,14 @@ class PodnapisiProvider(Provider):
             if series and season and episode:
                 subtitles.extend([PodnapisiSubtitle(language, int(s.find('id').text),
                                                     s.find('release').text.split() if s.find('release').text else [],
-                                                    'h' in (s.find('flags').text or ''), s.find('url').text,
+                                                    'n' in (s.find('flags').text or ''), s.find('url').text,
                                                     series=series, season=season, episode=episode,
                                                     year=s.find('year').text)
                                   for s in root.findall('subtitle')])
             elif title:
                 subtitles.extend([PodnapisiSubtitle(language, int(s.find('id').text),
                                                     s.find('release').text.split() if s.find('release').text else [],
-                                                    'h' in (s.find('flags').text or ''), s.find('url').text,
+                                                    'n' in (s.find('flags').text or ''), s.find('url').text,
                                                     title=title, year=s.find('year').text)
                                   for s in root.findall('subtitle')])
             if int(root.find('pagination/current').text) >= int(root.find('pagination/count').text):

3acbefb9c242083d892f46c66cb489c663fb5538
Chris Caron <lead2gold@gmail.com>
2015-01-25 19:57:26 -0500

    fixes #428; I'm not proud of this fix; but it resolves the issue. Hopefully the Addic7ed administrator can respond to my email inquiring as to why they are blocking us by the User-Agent string.
    Automation is the 21st century of the internet; No one wants to click 8
    times past banners just to get a 1KB (in size) subtitle. Most people
    have Ad blocking software and don't even see these banners anyway. There
    are many other ways to get people on board with helping them out
    financially (if that's what this is about), and at the same time
    accomodate those who've automated their service. I will roll back this
    commit when we can come to a better resolution.

--- subliminal/cli.py
+++ subliminal/cli.py
@@ -52,11 +52,6 @@ def subliminal():
     filtering_group.add_argument('-f', '--force', action='store_true',
                                  help='force subtitle download for videos with existing subtitles')
 
-    # addic7ed
-    addic7ed_group = parser.add_argument_group('addic7ed')
-    addic7ed_group.add_argument('--addic7ed-username', metavar='USERNAME', help='username for addic7ed provider')
-    addic7ed_group.add_argument('--addic7ed-password', metavar='PASSWORD', help='password for addic7ed provider')
-
     # output
     output_group = parser.add_argument_group('output')
     output_exclusive_group = output_group.add_mutually_exclusive_group()
@@ -100,11 +95,6 @@ def subliminal():
 
     # parse provider configs
     provider_configs = {}
-    if (args.addic7ed_username is not None and args.addic7ed_password is None
-        or args.addic7ed_username is None and args.addic7ed_password is not None):
-        parser.error('argument --addic7ed-username/--addic7ed-password: both arguments are required or none')
-    if args.addic7ed_username is not None and args.addic7ed_password is not None:
-        provider_configs['addic7ed'] = {'username': args.addic7ed_username, 'password': args.addic7ed_password}
 
     # parse color
     if args.color and colorlog is None:

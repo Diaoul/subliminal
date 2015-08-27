5258ef400fafb13e435de3e4842018f5fb93b179
Diaoul <diaoulael@gmail.com>
2015-08-27 17:06:13 +0200

    Code style

--- subliminal/cli.py
+++ subliminal/cli.py
@@ -205,9 +205,8 @@ config_file = 'config.ini'
 @click.option('--addic7ed', type=click.STRING, nargs=2, metavar='USERNAME PASSWORD', help='Addic7ed configuration.')
 @click.option('--opensubtitles', type=click.STRING, nargs=2, metavar='USERNAME PASSWORD',
               help='OpenSubtitles configuration.')
-@click.option('--cache-dir', type=click.Path(writable=True, resolve_path=True, file_okay=False),
-              default=app_dir, show_default=True, expose_value=True,
-              help='Path to the cache directory.')
+@click.option('--cache-dir', type=click.Path(writable=True, resolve_path=True, file_okay=False), default=app_dir,
+              show_default=True, expose_value=True, help='Path to the cache directory.')
 @click.option('--debug', is_flag=True, help='Print useful information for debugging subliminal and for reporting bugs.')
 @click.version_option(__version__)
 @click.pass_context

b0bd236b40ff438756ca386d5d30ec80336b800f
Antoine Bertin <diaoulael@gmail.com>
2016-02-09 17:25:18 +0100

    Add refiners

--- subliminal/cache.py
+++ subliminal/cache.py
@@ -9,5 +9,8 @@ SHOW_EXPIRATION_TIME = datetime.timedelta(weeks=3).total_seconds()
 #: Expiration time for episode caching
 EPISODE_EXPIRATION_TIME = datetime.timedelta(days=3).total_seconds()
 
+#: Expiration time for scraper searches
+REFINER_EXPIRATION_TIME = datetime.timedelta(weeks=1).total_seconds()
+
 
 region = make_region()

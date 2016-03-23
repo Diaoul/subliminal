152a5f12c30bb227e1b727270a4ca78b80acac79
Antoine Bertin <diaoulael@gmail.com>
2016-03-23 10:37:18 +0100

    Update docs usage cassette

--- docs/conftest.py
+++ docs/conftest.py
@@ -12,6 +12,7 @@ from vcr import VCR
 from subliminal.cache import region
 
 vcr = VCR(path_transformer=lambda path: path + '.yaml',
+          record_mode=os.environ.get('VCR_RECORD_MODE', 'once'),
           match_on=['method', 'scheme', 'host', 'port', 'path', 'query', 'body'],
           cassette_library_dir=os.path.join('docs', 'cassettes'))
 

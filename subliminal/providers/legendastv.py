5244e4fd792cf6dd49667be46572ede5f9c6c607
Rato <rato.aq2@gmail.com>
2016-10-10 00:03:18 +0200

    Drop archive name from matches: Fix for #691

--- subliminal/providers/legendastv.py
+++ subliminal/providers/legendastv.py
@@ -143,9 +143,6 @@ class LegendasTVSubtitle(Subtitle):
             if video.imdb_id and self.imdb_id == video.imdb_id:
                 matches.add('imdb_id')
 
-        # archive name
-        matches |= guess_matches(video, guessit(self.archive.name, {'type': self.type}))
-
         # name
         matches |= guess_matches(video, guessit(self.name, {'type': self.type}))
 

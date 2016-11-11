712cdb46ca0f72bea4a189e61244bfea42c12b5c
fernandog <fernandog@users.noreply.github.com>
2016-11-11 08:35:12 -0200

    Change to .net with https

--- subliminal/providers/podnapisi.py
+++ subliminal/providers/podnapisi.py
@@ -82,7 +82,7 @@ class PodnapisiProvider(Provider):
     """Podnapisi Provider."""
     languages = ({Language('por', 'BR'), Language('srp', script='Latn')} |
                  {Language.fromalpha2(l) for l in language_converters['alpha2'].codes})
-    server_url = 'http://podnapisi.eu/subtitles/'
+    server_url = 'https://podnapisi.net/subtitles/'
     subtitle_class = PodnapisiSubtitle
 
     def __init__(self):

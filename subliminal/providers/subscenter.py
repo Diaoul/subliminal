07fc2870d872535df0fc68f75c92a2a9982737fd
ofir123 <ofirbrukner@gmail.com>
2017-10-03 01:28:54 +0300

    Fixed subscenter provider!

--- subliminal/providers/subscenter.py
+++ subliminal/providers/subscenter.py
@@ -1,18 +1,16 @@
 # -*- coding: utf-8 -*-
 import bisect
-from collections import defaultdict
 import io
-import json
 import logging
+from urllib.parse import urlencode
 import zipfile
 
 from babelfish import Language
 from guessit import guessit
 from requests import Session
 
-from . import ParserBeautifulSoup, Provider
+from . import Provider
 from .. import __short_version__
-from ..cache import SHOW_EXPIRATION_TIME, region
 from ..exceptions import AuthenticationError, ConfigurationError, ProviderError
 from ..subtitle import Subtitle, fix_line_ending, guess_matches
 from ..utils import sanitize
@@ -25,16 +23,16 @@ class SubsCenterSubtitle(Subtitle):
     """SubsCenter Subtitle."""
     provider_name = 'subscenter'
 
-    def __init__(self, language, hearing_impaired, page_link, series, season, episode, title, subtitle_id, subtitle_key,
-                 downloaded, releases):
-        super(SubsCenterSubtitle, self).__init__(language, hearing_impaired, page_link)
+    def __init__(self, language, page_link, series, season, episode, title, subtitle_id, subtitle_key,
+                 releases):
+        super(SubsCenterSubtitle, self).__init__(language, page_link=page_link)
         self.series = series
         self.season = season
         self.episode = episode
         self.title = title
         self.subtitle_id = subtitle_id
         self.subtitle_key = subtitle_key
-        self.downloaded = downloaded
+        self.downloaded = 0
         self.releases = releases
 
     @property
@@ -74,17 +72,21 @@ class SubsCenterSubtitle(Subtitle):
 class SubsCenterProvider(Provider):
     """SubsCenter Provider."""
     languages = {Language.fromalpha2(l) for l in ['he']}
-    server_url = 'http://www.subscenter.co/he/'
+    server_url = 'http://www.cinemast.org/he/cinemast/api/'
     subtitle_class = SubsCenterSubtitle
+
+    default_username = 'subliminal@gmail.com'
+    default_password = 'subliminal'
 
     def __init__(self, username=None, password=None):
         if any((username, password)) and not all((username, password)):
             raise ConfigurationError('Username and password must be specified')
 
         self.session = None
-        self.username = username
-        self.password = password
-        self.logged_in = False
+        self.username = username or self.default_username
+        self.password = password or self.default_password
+        self.user_id = None
+        self.token = None
         self.session = None
 
     def initialize(self):
@@ -94,118 +96,95 @@ class SubsCenterProvider(Provider):
         # login
         if self.username and self.password:
             logger.debug('Logging in')
-            url = self.server_url + 'subscenter/accounts/login/'
-
-            # retrieve CSRF token
-            self.session.get(url)
-            csrf_token = self.session.cookies['csrftoken']
+            url = self.server_url + 'login/'
 
             # actual login
-            data = {'username': self.username, 'password': self.password, 'csrfmiddlewaretoken': csrf_token}
-            r = self.session.post(url, data, allow_redirects=False, timeout=10)
+            data = {'username': self.username, 'password': self.password}
+            r = self.session.post(url, data=urlencode(data), allow_redirects=False, timeout=10)
 
-            if r.status_code != 302:
+            if r.status_code != 200:
                 raise AuthenticationError(self.username)
 
-            logger.info('Logged in')
-            self.logged_in = True
+            try:
+                result = r.json()
+                if 'token' not in result:
+                    raise AuthenticationError(self.username)
+
+                logger.info('Logged in')
+                self.user_id = r.json().get('user')
+                self.token = r.json().get('token')
+            except ValueError:
+                raise AuthenticationError(self.username)
 
     def terminate(self):
         # logout
-        if self.logged_in:
-            logger.info('Logging out')
-            r = self.session.get(self.server_url + 'subscenter/accounts/logout/', timeout=10)
-            r.raise_for_status()
+        if self.token or self.user_id:
             logger.info('Logged out')
-            self.logged_in = False
+            self.token = None
+            self.user_id = None
 
         self.session.close()
 
-    @region.cache_on_arguments(expiration_time=SHOW_EXPIRATION_TIME)
-    def _search_url_titles(self, title):
-        """Search the URL titles by kind for the given `title`.
-
-        :param str title: title to search for.
-        :return: the URL titles by kind.
-        :rtype: collections.defaultdict
-
-        """
-        # make the search
-        logger.info('Searching title name for %r', title)
-        r = self.session.get(self.server_url + 'subtitle/search/', params={'q': title}, timeout=10)
-        r.raise_for_status()
-
-        # check for redirections
-        if r.history and all([h.status_code == 302 for h in r.history]):
-            logger.debug('Redirected to the subtitles page')
-            links = [r.url]
-        else:
-            # get the suggestions (if needed)
-            soup = ParserBeautifulSoup(r.content, ['lxml', 'html.parser'])
-            links = [link.attrs['href'] for link in soup.select('#processes div.generalWindowTop a')]
-            logger.debug('Found %d suggestions', len(links))
-
-        url_titles = defaultdict(list)
-        for link in links:
-            parts = link.split('/')
-            url_titles[parts[-3]].append(parts[-2])
-
-        return url_titles
-
-    def query(self, title, season=None, episode=None):
-        # search for the url title
-        url_titles = self._search_url_titles(title)
+    def query(self, title, season=None, episode=None, year=None):
+        query = {
+            'q': title,
+            'user': self.user_id,
+            'token': self.token
+        }
 
         # episode
         if season and episode:
-            if 'series' not in url_titles:
-                logger.error('No URL title found for series %r', title)
-                return []
-            url_title = url_titles['series'][0]
-            logger.debug('Using series title %r', url_title)
-            url = self.server_url + 'cst/data/series/sb/{}/{}/{}/'.format(url_title, season, episode)
-            page_link = self.server_url + 'subtitle/series/{}/{}/{}/'.format(url_title, season, episode)
+            query['type'] = 'series'
+            query['season'] = season
+            query['episode'] = episode
         else:
-            if 'movie' not in url_titles:
-                logger.error('No URL title found for movie %r', title)
-                return []
-            url_title = url_titles['movie'][0]
-            logger.debug('Using movie title %r', url_title)
-            url = self.server_url + 'cst/data/movie/sb/{}/'.format(url_title)
-            page_link = self.server_url + 'subtitle/movie/{}/'.format(url_title)
+            query['type'] = 'movies'
+            if year:
+                query['year_start'] = year - 1
+                query['year_end'] = year
 
         # get the list of subtitles
         logger.debug('Getting the list of subtitles')
-        r = self.session.get(url)
+        url = self.server_url + 'search/'
+        r = self.session.post(url, data=urlencode(query))
         r.raise_for_status()
-        results = json.loads(r.text)
+
+        try:
+            results = r.json()
+        except ValueError:
+            return {}
 
         # loop over results
         subtitles = {}
-        for language_code, language_data in results.items():
-            for quality_data in language_data.values():
-                for quality, subtitles_data in quality_data.items():
-                    for subtitle_item in subtitles_data.values():
-                        # read the item
-                        language = Language.fromalpha2(language_code)
-                        hearing_impaired = bool(subtitle_item['hearing_impaired'])
-                        subtitle_id = subtitle_item['id']
-                        subtitle_key = subtitle_item['key']
-                        downloaded = subtitle_item['downloaded']
-                        release = subtitle_item['subtitle_version']
+        for group_data in results.get('data', []):
+            # create page link
+            slug_name = group_data.get('name_en').lower().replace(' ', '-').replace('\'', '').replace('"', '')
+            if query['type'] == 'series':
+                page_link = self.server_url + 'subtitle/series/{}/{}/{}/'.format(slug_name, season, episode)
+            else:
+                page_link = self.server_url + 'subtitle/movie/{}/'.format(slug_name)
 
-                        # add the release and increment downloaded count if we already have the subtitle
-                        if subtitle_id in subtitles:
-                            logger.debug('Found additional release %r for subtitle %d', release, subtitle_id)
-                            bisect.insort_left(subtitles[subtitle_id].releases, release)  # deterministic order
-                            subtitles[subtitle_id].downloaded += downloaded
-                            continue
+            # go over each language
+            for language_code, subtitles_data in group_data.get('subtitles', {}).items():
+                for subtitle_item in subtitles_data:
+                    # read the item
+                    language = Language.fromalpha2(language_code)
+                    subtitle_id = subtitle_item['id']
+                    subtitle_key = subtitle_item['key']
+                    release = subtitle_item['version']
 
-                        # otherwise create it
-                        subtitle = self.subtitle_class(language, hearing_impaired, page_link, title, season, episode,
-                                                       title, subtitle_id, subtitle_key, downloaded, [release])
-                        logger.debug('Found subtitle %r', subtitle)
-                        subtitles[subtitle_id] = subtitle
+                    # add the release and increment downloaded count if we already have the subtitle
+                    if subtitle_id in subtitles:
+                        logger.debug('Found additional release %r for subtitle %r', release, subtitle_id)
+                        bisect.insort_left(subtitles[subtitle_id].releases, release)  # deterministic order
+                        subtitles[subtitle_id].downloaded += 1
+                        continue
+
+                    # otherwise create it
+                    subtitle = self.subtitle_class(language, page_link, title, season, episode, title, subtitle_id,
+                                                   subtitle_key, [release])
+                    logger.debug('Found subtitle %r', subtitle)
+                    subtitles[subtitle_id] = subtitle
 
         return subtitles.values()
 
@@ -222,9 +201,17 @@ class SubsCenterProvider(Provider):
 
     def download_subtitle(self, subtitle):
         # download
-        url = self.server_url + 'subtitle/download/{}/{}/'.format(subtitle.language.alpha2, subtitle.subtitle_id)
-        params = {'v': subtitle.releases[0], 'key': subtitle.subtitle_key}
-        r = self.session.get(url, params=params, headers={'Referer': subtitle.page_link}, timeout=10)
+        url = self.server_url + 'subtitle/download/{}/'.format(subtitle.language.alpha2)
+        params = {
+            'v': subtitle.releases[0],
+            'key': subtitle.subtitle_key,
+            'sub_id': subtitle.subtitle_id
+        }
+        data = {
+            'user': self.user_id,
+            'token': self.token
+        }
+        r = self.session.post(url, data=urlencode(data), params=params, timeout=10)
         r.raise_for_status()
 
         # open the zip
--- tests/test_subscenter.py
+++ tests/test_subscenter.py
@@ -19,24 +19,24 @@ def test_get_matches_movie(movies):
     releases = ['Enders.Game.2013.HC.Webrip.x264.AC3-TiTAN', 'Enders.Game.2013.KORSUB.HDRip.XViD-NO1KNOWS',
                 'Enders.Game.2013.KORSUB.HDRip.h264.AAC-RARBG', 'Enders.Game.2013.HDRip.XViD.AC3-ReLeNTLesS',
                 'Enders.Game.2013.720p.KOR.HDRip.H264-KTH']
-    subtitle = SubsCenterSubtitle(Language('heb'), False, None, None, None, None, 'Ender\'s Game', 266898,
-                                  '54adce017db2e7fd8501b7a321451b64', 389, releases)
+    subtitle = SubsCenterSubtitle(Language('heb'), None, None, None, None, 'Ender\'s Game', 266898,
+                                  '54adce017db2e7fd8501b7a321451b64', releases)
     matches = subtitle.get_matches(movies['enders_game'])
     assert matches == {'title', 'year', 'resolution', 'video_codec'}
 
 
 def test_get_matches_episode(episodes):
     releases = ['Game.of.Thrones.S03E10.HDTV.x264-EVOLVE']
-    subtitle = SubsCenterSubtitle(Language('heb'), False, None, 'Game of Thrones', 3, 10, 'Mhysa', 263129,
-                                  '6a3129e8b9effdb8231aa6b3caf66fbe', 6706, releases)
+    subtitle = SubsCenterSubtitle(Language('heb'), None, 'Game of Thrones', 3, 10, 'Mhysa', 263129,
+                                  '6a3129e8b9effdb8231aa6b3caf66fbe', releases)
     matches = subtitle.get_matches(episodes['got_s03e10'])
     assert matches == {'series', 'episode', 'season', 'title', 'year', 'video_codec'}
 
 
 def test_get_matches_no_match(movies):
     releases = ['Game.of.Thrones.S03E10.HDTV.EVOLVE']
-    subtitle = SubsCenterSubtitle(Language('heb'), False, None, 'Game of Thrones', 3, 10, 'Mhysa', 263129,
-                                  '6a3129e8b9effdb8231aa6b3caf66fbe', 6706, releases)
+    subtitle = SubsCenterSubtitle(Language('heb'), None, 'Game of Thrones', 3, 10, 'Mhysa', 263129,
+                                  '6a3129e8b9effdb8231aa6b3caf66fbe', releases)
     matches = subtitle.get_matches(movies['man_of_steel'])
     assert matches == set()
 
@@ -55,9 +55,9 @@ def test_configuration_error_no_password():
 @vcr.use_cassette
 def test_login():
     provider = SubsCenterProvider('subliminal@gmail.com', 'subliminal')
-    assert not provider.logged_in
+    assert not provider.token
     provider.initialize()
-    assert provider.logged_in
+    assert provider.token
 
 
 @pytest.mark.integration
@@ -74,39 +74,7 @@ def test_logout():
     provider = SubsCenterProvider('subliminal@gmail.com', 'subliminal')
     provider.initialize()
     provider.terminate()
-    assert not provider.logged_in
-
-
-@pytest.mark.integration
-@vcr.use_cassette
-def test_search_url_titles_episode(episodes):
-    video = episodes['dallas_2012_s01e03']
-    with SubsCenterProvider() as provider:
-        url_titles = provider._search_url_titles(video.series)
-    assert 'movie' in url_titles
-    assert 'series' in url_titles
-    assert len(url_titles['series']) == 1
-    assert url_titles['series'][0] == 'dallas'
-
-
-@pytest.mark.integration
-@vcr.use_cassette
-def test_search_url_titles_movies(movies):
-    video = movies['man_of_steel']
-    with SubsCenterProvider() as provider:
-        url_titles = provider._search_url_titles(video.title)
-    assert 'movie' in url_titles
-    assert 'series' not in url_titles
-    assert len(url_titles['movie']) == 1
-    assert url_titles['movie'][0] == 'superman-man-of-steel'
-
-
-@pytest.mark.integration
-@vcr.use_cassette
-def test_search_url_titles_no_suggestion():
-    with SubsCenterProvider() as provider:
-        url_titles = provider._search_url_titles('DCs Legends of Tomorrow')
-    assert len(url_titles) == 0
+    assert not provider.token
 
 
 @pytest.mark.integration
@@ -150,7 +118,7 @@ def test_list_subtitles_movie(movies):
 def test_list_subtitles_episode(episodes):
     video = episodes['got_s03e10']
     languages = {Language('heb')}
-    expected_subtitles = {'263128', '263129', '263127', '263139', '263130'}
+    expected_subtitles = {'263128', '290133', '263129', '263127', '263139', '263130'}
     with SubsCenterProvider() as provider:
         subtitles = provider.list_subtitles(video, languages)
     assert len(subtitles) == len(expected_subtitles)

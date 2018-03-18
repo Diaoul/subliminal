27700a42b0ea93e737664426b93b51019038d525
h3llrais3r <pooh_beer_1@hotmail.com>
2018-03-18 11:55:50 +0100

    Use custom dogpile cache key generator
    Dogpile cache cache only accepts byte strings as values for the default key generator. Passing non ascii characters results in `UnicodeEncodeError`.
    By creating a custom key generator which converts unicode strings to byte strings, error is no longer thrown.
    Fixes https://github.com/Diaoul/subliminal/issues/887 and also other `cache_on_arguments` calls where unicode strings are passed with non ascii characters.

--- subliminal/cache.py
+++ subliminal/cache.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 import datetime
 
+import six
 from dogpile.cache import make_region
+from dogpile.cache.util import function_key_generator
 
 #: Expiration time for show caching
 SHOW_EXPIRATION_TIME = datetime.timedelta(weeks=3).total_seconds()
@@ -13,4 +15,15 @@ EPISODE_EXPIRATION_TIME = datetime.timedelta(days=3).total_seconds()
 REFINER_EXPIRATION_TIME = datetime.timedelta(weeks=1).total_seconds()
 
 
-region = make_region()
+def _to_byte_str(value):
+    if isinstance(value, six.text_type):
+        return value.encode('utf-8')
+    else:
+        return six.binary_type(value)
+
+
+def _to_byte_str_key_generator(namespace, fn, to_str=_to_byte_str):
+    return function_key_generator(namespace, fn, to_str)
+
+
+region = make_region(function_key_generator=_to_byte_str_key_generator)

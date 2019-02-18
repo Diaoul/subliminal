d7a8e6ced5ce47d6796a6a0b5258c9626624d42e
Panagiotis Koutsias <me@pkoutsias.com>
2019-02-18 12:38:28 +0200

    Caches suggestions only when results are found

--- subliminal/providers/subs4free.py
+++ subliminal/providers/subs4free.py
@@ -121,7 +121,8 @@ class Subs4FreeProvider(Provider):
 
         return matched_show_ids
 
-    @region.cache_on_arguments(expiration_time=SHOW_EXPIRATION_TIME, to_str=text_type)
+    @region.cache_on_arguments(expiration_time=SHOW_EXPIRATION_TIME, to_str=text_type,
+                               should_cache_fn=lambda value: value)
     def _get_suggestions(self, title):
         """Search the show or movie id from the `title` and `year`.
 

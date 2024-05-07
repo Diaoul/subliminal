c017edbf1ee8c8be5952a50ac4ba78039363e86e
getzze <getzze@gmail.com>
2024-05-08 00:45:51 +0100

    remove python2 stuff from subliminal - 4

--- docs/conftest.py
+++ docs/conftest.py
@@ -1,11 +1,8 @@
 import os
 import sys
+from unittest.mock import Mock
 
 import pytest
-try:
-    from unittest.mock import Mock
-except ImportError:
-    from mock import Mock
 from vcr import VCR
 
 from subliminal.cache import region
@@ -27,13 +24,7 @@ def chdir(tmpdir, monkeypatch):
     monkeypatch.chdir(str(tmpdir))
 
 
-@pytest.yield_fixture(autouse=True)
+@pytest.fixture(autouse=True)
 def use_cassette(request):
     with vcr.use_cassette('test_' + request.fspath.purebasename):
         yield
-
-
-@pytest.fixture(autouse=True)
-def skip_python_2():
-    if sys.version_info < (3, 0):
-        return pytest.skip('Requires python 3')

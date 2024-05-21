83f3b183f8c101926fbfc349f29cd1e94677c069
getzze <getzze@gmail.com>
2024-05-21 13:21:03 +0100

    Add type hint to providers/__init__.py

--- subliminal/providers/__init__.py
+++ subliminal/providers/__init__.py
@@ -1,40 +1,82 @@
+"""
+Providers list and download subtitles for a :class:`~subliminal.video.Video` object.
+
+A Provider is a ContextManager with `__enter__` and `__exit__` methods and two public methods:
+
+.. py:method:: list_subtitles(self, video, languages)
+
+    :param video: video to list subtitles for.
+    :type video: :class:`~subliminal.video.Video`
+    :param languages: languages to search for.
+    :type languages: set of :class:`~babelfish.language.Language`
+    :return: found subtitles.
+    :rtype: list of :class:`~subliminal.subtitle.Subtitle`
+    :raise: :class:`~subliminal.exceptions.ProviderError`
+
+
+.. py:method:: download_subtitle(self, subtitle)
+
+    :param subtitle: subtitle to download.
+    :type subtitle: :class:`~subliminal.subtitle.Subtitle`
+    :raise: :class:`~subliminal.exceptions.ProviderError`
+
+"""
+
 from __future__ import annotations
 
 import logging
 import ssl
+from typing import TYPE_CHECKING, Any, ClassVar, Generic, TypeVar
 from xmlrpc.client import SafeTransport
 
 from bs4 import BeautifulSoup, FeatureNotFound
 from requests import adapters
-from urllib3 import poolmanager
+from urllib3 import poolmanager  # type: ignore[import-untyped]
 
-from .. import __short_version__
-from ..video import Episode, Movie
+from subliminal import __short_version__
+from subliminal.subtitle import Subtitle
+from subliminal.video import Episode, Movie, Video
+
+if TYPE_CHECKING:
+    from collections.abc import Sequence, Set
+    from http.client import HTTPSConnection
+    from types import TracebackType
+    from typing import Self
+    from xmlrpc.client import _HostType
+
+    from babelfish import Language  # type: ignore[import-untyped]
+
 
 logger = logging.getLogger(__name__)
 
 
 class SecLevelOneTLSAdapter(adapters.HTTPAdapter):
+    """``HTTPAdapter`` with security level set to 1."""
 
-    def init_poolmanager(self, connections, maxsize, block=False):
+    def init_poolmanager(self, connections: int, maxsize: int, block: bool = False, **pool_kwargs: Any) -> None:  # noqa: FBT001, FBT002
         """Create and initialize the urllib3 PoolManager."""
         ctx = ssl.create_default_context()
         ctx.set_ciphers('DEFAULT@SECLEVEL=1')
         self.poolmanager = poolmanager.PoolManager(
-                num_pools=connections,
-                maxsize=maxsize,
-                block=block,
-                ssl_version=ssl.PROTOCOL_TLS,
-                ssl_context=ctx)
+            num_pools=connections,
+            maxsize=maxsize,
+            block=block,
+            ssl_version=ssl.PROTOCOL_TLS,
+            ssl_context=ctx,
+        )
 
 
 class TimeoutSafeTransport(SafeTransport):
     """Timeout support for ``xmlrpc.client.SafeTransport``."""
-    def __init__(self, timeout, *args, **kwargs):
+
+    timeout: float | None
+
+    def __init__(self, timeout: float | None, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
         self.timeout = timeout
 
-    def make_connection(self, host):
+    def make_connection(self, host: _HostType) -> HTTPSConnection:
+        """Make connection to host."""
         c = SafeTransport.make_connection(self, host)
         c.timeout = self.timeout
 
@@ -48,59 +90,75 @@ class ParserBeautifulSoup(BeautifulSoup):
     :param list parsers: parser names, in order of preference.
 
     """
-    def __init__(self, markup, parsers, **kwargs):
+
+    def __init__(self, markup: str | bytes, parsers: Sequence[str], **kwargs: Any) -> None:
         # reject features
         if set(parsers).intersection({'fast', 'permissive', 'strict', 'xml', 'html', 'html5'}):
-            raise ValueError('Features not allowed, only parser names')
+            msg = 'Features not allowed, only parser names'
+            raise ValueError(msg)
 
         # reject some kwargs
         if 'features' in kwargs:
-            raise ValueError('Cannot use features kwarg')
+            msg = 'Cannot use features kwarg'
+            raise ValueError(msg)
         if 'builder' in kwargs:
-            raise ValueError('Cannot use builder kwarg')
+            msg = 'Cannot use builder kwarg'
+            raise ValueError(msg)
 
         # pick the first parser available
         for parser in parsers:
             try:
                 super().__init__(markup, parser, **kwargs)
-                return
             except FeatureNotFound:
                 pass
+            else:
+                return
 
         raise FeatureNotFound
 
 
-class Provider:
+S = TypeVar('S', bound=Subtitle)
+
+
+class Provider(Generic[S]):
     """Base class for providers.
+
+    Each Provider returns subtitles from a specialized subclass of :class:`~subliminal.subtitle.Subtitle`.
 
     If any configuration is possible for the provider, like credentials, it must take place during instantiation.
 
     :raise: :class:`~subliminal.exceptions.ConfigurationError` if there is a configuration error
 
     """
+
     #: Supported set of :class:`~babelfish.language.Language`
-    languages = set()
+    languages: ClassVar[Set[Language]] = frozenset()
 
     #: Supported video types
-    video_types = (Episode, Movie)
+    video_types: ClassVar[tuple[type[Video], ...]] = (Episode, Movie)
 
     #: Required hash, if any
-    required_hash = None
+    required_hash: ClassVar[str | None] = None
 
     #: Subtitle class to use
-    subtitle_class = None
+    subtitle_class: ClassVar[type[S] | None] = None  # type: ignore[misc]
 
     #: User Agent to use
-    user_agent = 'Subliminal/%s' % __short_version__
+    user_agent: str = f'Subliminal/{__short_version__}'
 
-    def __enter__(self):
+    def __enter__(self) -> Self:
         self.initialize()
         return self
 
-    def __exit__(self, exc_type, exc_value, traceback):
+    def __exit__(
+        self,
+        exc_type: type[BaseException],
+        exc_value: BaseException | None,
+        traceback: TracebackType | None,
+    ) -> None:
         self.terminate()
 
-    def initialize(self):
+    def initialize(self) -> None:
         """Initialize the provider.
 
         Must be called when starting to work with the provider. This is the place for network initialization
@@ -112,7 +170,7 @@ class Provider:
         """
         raise NotImplementedError
 
-    def terminate(self):
+    def terminate(self) -> None:
         """Terminate the provider.
 
         Must be called when done with the provider. This is the place for network shutdown or logout operations.
@@ -124,7 +182,7 @@ class Provider:
         raise NotImplementedError
 
     @classmethod
-    def check(cls, video):
+    def check(cls, video: Video) -> bool:
         """Check if the `video` can be processed.
 
         The `video` is considered invalid if not an instance of :attr:`video_types` or if the :attr:`required_hash` is
@@ -144,7 +202,7 @@ class Provider:
         return True
 
     @classmethod
-    def check_types(cls, video):
+    def check_types(cls, video: Video) -> bool:
         """Check if the `video` type is supported by the provider.
 
         The `video` is considered invalid if not an instance of :attr:`video_types`.
@@ -158,7 +216,7 @@ class Provider:
         return isinstance(video, cls.video_types)
 
     @classmethod
-    def check_languages(cls, languages):
+    def check_languages(cls, languages: Set[Language]) -> Set[Language]:
         """Check if the `languages` are supported by the provider.
 
         A subset of the supported languages is returned.
@@ -171,7 +229,7 @@ class Provider:
         """
         return cls.languages & languages
 
-    def query(self, *args, **kwargs):
+    def query(self, *args: Any, **kwargs: Any) -> list[S]:
         """Query the provider for subtitles.
 
         Arguments should match as much as possible the actual parameters for querying the provider
@@ -183,7 +241,7 @@ class Provider:
         """
         raise NotImplementedError
 
-    def list_subtitles(self, video, languages):
+    def list_subtitles(self, video: Video, languages: Set[Language]) -> list[S]:
         """List subtitles for the `video` with the given `languages`.
 
         This will call the :meth:`query` method internally. The parameters passed to the :meth:`query` method may
@@ -200,7 +258,7 @@ class Provider:
         """
         raise NotImplementedError
 
-    def download_subtitle(self, subtitle):
+    def download_subtitle(self, subtitle: S) -> None:
         """Download `subtitle`'s :attr:`~subliminal.subtitle.Subtitle.content`.
 
         :param subtitle: subtitle to download.
@@ -210,5 +268,5 @@ class Provider:
         """
         raise NotImplementedError
 
-    def __repr__(self):
-        return '<%s [%r]>' % (self.__class__.__name__, self.video_types)
+    def __repr__(self) -> str:
+        return f'<{self.__class__.__name__} [{self.video_types!r}]>'

d35538924748029d759a5170f8d77d6902b6d26a
getzze <getzze@gmail.com>
2024-05-21 13:56:11 +0100

    Add type hint to exceptions.py

--- subliminal/exceptions.py
+++ subliminal/exceptions.py
@@ -1,33 +1,43 @@
+"""Exceptions for Subliminal."""
+
+
 class Error(Exception):
     """Base class for exceptions in subliminal."""
+
     pass
 
 
 class ProviderError(Error):
     """Exception raised by providers."""
+
     pass
 
 
 class NotInitializedProviderError(ProviderError):
     """Exception raised by providers when not initialized."""
+
     pass
 
 
 class ConfigurationError(ProviderError):
     """Exception raised by providers when badly configured."""
+
     pass
 
 
 class AuthenticationError(ProviderError):
     """Exception raised by providers when authentication failed."""
+
     pass
 
 
 class ServiceUnavailable(ProviderError):
     """Exception raised when status is '503 Service Unavailable'."""
+
     pass
 
 
 class DownloadLimitExceeded(ProviderError):
     """Exception raised by providers when download limit is exceeded."""
+
     pass

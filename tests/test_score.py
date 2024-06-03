a7400d97ee1beb4a3d0c0957a6085fb66b452246
getzze <getzze@gmail.com>
2024-06-03 23:02:57 +0100

    Lint and type hints

--- tests/test_score.py
+++ tests/test_score.py
@@ -1,5 +1,6 @@
-from babelfish import Language
+from __future__ import annotations
 
+from babelfish import Language  # type: ignore[import-untyped]
 from subliminal.providers.addic7ed import Addic7edSubtitle
 from subliminal.providers.opensubtitles import OpenSubtitlesSubtitle
 from subliminal.providers.podnapisi import PodnapisiSubtitle
@@ -24,34 +25,74 @@ def test_movie_equations():
 
 def test_compute_score(episodes):
     video = episodes['bbt_s07e05']
-    subtitle = Addic7edSubtitle(Language('eng'), True, None, 'the big BANG theory', 6, 4, None, None, '1080p', None)
+    subtitle = Addic7edSubtitle(
+        language=Language('eng'),
+        hearing_impaired=True,
+        page_link=None,
+        series='the big BANG theory',
+        season=6,
+        episode=4,
+        title=None,
+        year=None,
+        release_group='1080p',
+        download_link='',
+    )
     expected_score = episode_scores['series'] + episode_scores['year'] + episode_scores['country']
     assert compute_score(subtitle, video) == expected_score
 
 
 def test_get_score_cap(movies):
     video = movies['man_of_steel']
-    subtitle = OpenSubtitlesSubtitle(Language('eng'), True, None, 1, 'hash', 'movie', '5b8f8f4e41ccb21e',
-                                     'Man of Steel', 'man.of.steel.2013.720p.bluray.x264-felony.mkv', 2013, 770828,
-                                     None, None, '', 'utf-8')
+    subtitle = OpenSubtitlesSubtitle(
+        language=Language('eng'),
+        subtitle_id='1',
+        hearing_impaired=True,
+        page_link=None,
+        matched_by='hash',
+        movie_kind='movie',
+        moviehash='5b8f8f4e41ccb21e',
+        movie_name='Man of Steel',
+        movie_release_name='man.of.steel.2013.720p.bluray.x264-felony.mkv',
+        movie_year=2013,
+        movie_imdb_id='tt770828',
+        series_season=None,
+        series_episode=None,
+        filename='',
+        encoding='utf-8',
+    )
     assert compute_score(subtitle, video) == movie_scores['hash']
 
 
 def test_compute_score_episode_imdb_id(movies):
     video = movies['man_of_steel']
-    subtitle = OpenSubtitlesSubtitle(Language('eng'), True, None, 1, 'hash', 'movie', None,
-                                     'Man of Steel', 'man.of.steel.2013.720p.bluray.x264-felony.mkv', 2013, 770828,
-                                     None, None, '', 'utf-8')
-    assert compute_score(subtitle, video) == sum(movie_scores.get(m, 0) for m in
-                                                 ('imdb_id', 'title', 'year', 'country', 'release_group', 'source',
-                                                  'resolution', 'video_codec'))
+    subtitle = OpenSubtitlesSubtitle(
+        language=Language('eng'),
+        subtitle_id='1',
+        hearing_impaired=True,
+        page_link=None,
+        matched_by='hash',
+        movie_kind='movie',
+        moviehash=None,
+        movie_name='Man of Steel',
+        movie_release_name='man.of.steel.2013.720p.bluray.x264-felony.mkv',
+        movie_year=2013,
+        movie_imdb_id='tt770828',
+        series_season=None,
+        series_episode=None,
+        filename='',
+        encoding='utf-8',
+    )
+    assert compute_score(subtitle, video) == sum(
+        movie_scores.get(m, 0)
+        for m in ('imdb_id', 'title', 'year', 'country', 'release_group', 'source', 'resolution', 'video_codec')
+    )
 
 
 def test_compute_score_episode_title(episodes):
     video = episodes['bbt_s07e05']
     subtitle = PodnapisiSubtitle(
         language=Language('eng'),
-        subtitle_id="1",
+        subtitle_id='1',
         hearing_impaired=True,
         page_link=None,
         releases=['The.Big.Bang.Theory.S07E05.The.Workplace.Proximity.720p.HDTV.x264-DIMENSION.mkv'],
@@ -60,15 +101,42 @@ def test_compute_score_episode_title(episodes):
         episode=5,
         year=None,
     )
-    assert compute_score(subtitle, video) == sum(episode_scores.get(m, 0) for m in
-                                                 ('series', 'year', 'country', 'season', 'episode', 'release_group',
-                                                  'source', 'resolution', 'video_codec', 'title'))
+    assert compute_score(subtitle, video) == sum(
+        episode_scores.get(m, 0)
+        for m in (
+            'series',
+            'year',
+            'country',
+            'season',
+            'episode',
+            'release_group',
+            'source',
+            'resolution',
+            'video_codec',
+            'title',
+        )
+    )
 
 
 def test_compute_score_hash_hearing_impaired(movies):
     video = movies['man_of_steel']
-    subtitle = OpenSubtitlesSubtitle(Language('eng'), True, None, 1, 'hash', 'movie', '5b8f8f4e41ccb21e',
-                                     'Man of Steel', 'man.of.steel.2013.720p.bluray.x264-felony.mkv', 2013, 770828,
-                                     None, None, '', 'utf-8')
-    assert compute_score(subtitle, video, hearing_impaired=True) == (movie_scores['hash'] +
-                                                                     movie_scores['hearing_impaired'])
+    subtitle = OpenSubtitlesSubtitle(
+        language=Language('eng'),
+        subtitle_id='1',
+        hearing_impaired=True,
+        page_link=None,
+        matched_by='hash',
+        movie_kind='movie',
+        moviehash='5b8f8f4e41ccb21e',
+        movie_name='Man of Steel',
+        movie_release_name='man.of.steel.2013.720p.bluray.x264-felony.mkv',
+        movie_year=2013,
+        movie_imdb_id='tt770828',
+        series_season=None,
+        series_episode=None,
+        filename='',
+        encoding='utf-8',
+    )
+    assert compute_score(subtitle, video, hearing_impaired=True) == (
+        movie_scores['hash'] + movie_scores['hearing_impaired']
+    )
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,4 +2,4 @@ from subliminal.utils import sanitize
 
 
 def test_sanitize():
-    assert sanitize('Marvel\'s Agents of S.H.I.E.L.D.') == 'marvels agents of s h i e l d'
+    assert sanitize("Marvel's Agents of S.H.I.E.L.D.") == 'marvels agents of s h i e l d'

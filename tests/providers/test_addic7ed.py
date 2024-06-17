b29b700cd7f6127da90214400f95f0fa7fba76e0
getzze <getzze@gmail.com>
2024-06-17 10:57:05 +0100

    add decode_compressed_response=True

--- tests/providers/test_addic7ed.py
+++ tests/providers/test_addic7ed.py
@@ -9,8 +9,9 @@ from vcr import VCR  # type: ignore[import-untyped]
 vcr = VCR(
     path_transformer=lambda path: path + '.yaml',
     record_mode=os.environ.get('VCR_RECORD_MODE', 'once'),
+    decode_compressed_response=True,
     match_on=['method', 'scheme', 'host', 'port', 'path', 'query', 'body'],
-    cassette_library_dir=os.path.join('tests', 'cassettes', 'addic7ed'),
+    cassette_library_dir=os.path.realpath(os.path.join('tests', 'cassettes', 'addic7ed')),
 )
 
 USERNAME = 'subliminal'
--- tests/providers/test_tvsubtitles.py
+++ tests/providers/test_tvsubtitles.py
@@ -8,8 +8,9 @@ from vcr import VCR  # type: ignore[import-untyped]
 vcr = VCR(
     path_transformer=lambda path: path + '.yaml',
     record_mode=os.environ.get('VCR_RECORD_MODE', 'once'),
+    decode_compressed_response=True,
     match_on=['method', 'scheme', 'host', 'port', 'path', 'query', 'body'],
-    cassette_library_dir=os.path.realpath(os.path.realpath(os.path.join('tests', 'cassettes', 'tvsubtitles'))),
+    cassette_library_dir=os.path.realpath(os.path.join('tests', 'cassettes', 'tvsubtitles')),
 )
 
 

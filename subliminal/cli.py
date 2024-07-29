cc12c9f0a73423296f7fcc0ead2d7fa27bd02de7
getzze <getzze@gmail.com>
2024-07-30 00:00:49 +0100

    Allow to use the latest of creation and modification date to compute the age of the file.

--- subliminal/cli.py
+++ subliminal/cli.py
@@ -308,6 +308,16 @@ def cache(ctx: click.Context, clear_subliminal: bool) -> None:
 @click.option('-r', '--refiner', type=REFINER, multiple=True, help='Refiner to use (can be used multiple times).')
 @click.option('-a', '--age', type=AGE, help='Filter videos newer than AGE, e.g. 12h, 1w2d.')
 @click.option(
+    '--use_creation_time',
+    'use_ctime',
+    is_flag=True,
+    default=False,
+    help=(
+        'Use the latest of modification date and creation date to calculate the age. '
+        'Otherwise, just use the modification date.'
+    ),
+)
+@click.option(
     '-d',
     '--directory',
     type=click.STRING,
@@ -364,6 +374,7 @@ def download(
     refiner: Sequence[str],
     language: Sequence[Language],
     age: timedelta | None,
+    use_ctime: bool,
     directory: str | None,
     encoding: str | None,
     original_encoding: bool,
@@ -421,7 +432,7 @@ def download(
                 if not force:
                     video.subtitles |= set(search_external_subtitles(video.name, directory=directory).values())
 
-                if check_video(video, languages=language_set, age=age, undefined=single):
+                if check_video(video, languages=language_set, age=age, use_ctime=use_ctime, undefined=single):
                     refine(
                         video,
                         episode_refiners=refiner,
@@ -437,7 +448,7 @@ def download(
             # directories
             if os.path.isdir(p):
                 try:
-                    scanned_videos = scan_videos(p, age=age, archives=archives)
+                    scanned_videos = scan_videos(p, age=age, use_ctime=use_ctime, archives=archives)
                 except ValueError:
                     logger.exception('Unexpected error while collecting directory path %s', p)
                     errored_paths.append(p)
@@ -445,7 +456,7 @@ def download(
                 for video in scanned_videos:
                     if not force:
                         video.subtitles |= set(search_external_subtitles(video.name, directory=directory).values())
-                    if check_video(video, languages=language_set, age=age, undefined=single):
+                    if check_video(video, languages=language_set, age=age, use_ctime=use_ctime, undefined=single):
                         refine(
                             video,
                             episode_refiners=refiner,
@@ -469,7 +480,7 @@ def download(
                 continue
             if not force:
                 video.subtitles |= set(search_external_subtitles(video.name, directory=directory).values())
-            if check_video(video, languages=language_set, age=age, undefined=single):
+            if check_video(video, languages=language_set, age=age, use_ctime=use_ctime, undefined=single):
                 refine(
                     video,
                     episode_refiners=refiner,
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,22 @@
-from subliminal.utils import sanitize
+from datetime import timedelta
+
+from subliminal.utils import creation_date, get_age, modification_date, sanitize
 
 
 def test_sanitize():
     assert sanitize("Marvel's Agents of S.H.I.E.L.D.") == 'marvels agents of s h i e l d'
+
+
+def test_get_age() -> None:
+    age = get_age(__file__)
+    assert age > timedelta()
+
+    c_age = get_age(__file__, use_ctime=True)
+    assert c_age > timedelta()
+
+    not_file_age = get_age('not-a-file.txt')
+    assert not_file_age == timedelta()
+
+    creation_or_modification = creation_date(__file__)
+    modification = modification_date(__file__)
+    assert creation_or_modification >= modification

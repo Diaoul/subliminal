b3a9c2aaa99f2e028fc28338ae6069cddffac5b9
getzze <getzze@gmail.com>
2024-11-22 23:29:35 +0000

    build docs with installed package

--- docs/conf.py
+++ docs/conf.py
@@ -12,14 +12,9 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys
-import os
-import sphinx_rtd_theme
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('..'))
 import subliminal
 
 # -- General configuration ------------------------------------------------

473154111b7ef8db4c1699d162424bb4188937ca
getzze <getzze@gmail.com>
2024-12-12 23:18:21 +0000

    use dedent

--- src/subliminal/__main__.py
+++ src/subliminal/__main__.py
@@ -1,17 +1,23 @@
+"""Module subliminal."""
+
 from __future__ import annotations
+
+from textwrap import dedent
 
 if len(__package__) == 0:
     import sys
 
-    print(
-        f"""
+    print(  # noqa: T201
+        dedent(
+            f"""
 
-The '__main__' module does not seem to have been run in the context of a
-runnable package ... did you forget to add the '-m' flag?
+            The '__main__' module does not seem to have been run in the context
+            of a runnable package ... did you forget to add the '-m' flag?
 
-Usage: {sys.executable} -m subliminal {' '.join(sys.argv[1:])}
+            Usage: {sys.executable} -m subliminal {' '.join(sys.argv[1:])}
 
-"""
+            """
+        )
     )
     sys.exit(2)
 

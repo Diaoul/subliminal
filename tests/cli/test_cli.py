2ed3e840b18c4793da8b53a1f3c915805eaa41d3
getzze <getzze@gmail.com>
2025-12-09 22:12:46 +0000

    create a minimal CliRunner class with pytest fixtures

--- tests/cli/test_cli.py
+++ tests/cli/test_cli.py
@@ -1,30 +1,34 @@
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
 import pytest
-from click.testing import CliRunner
 
 from subliminal.cli.cli import subliminal as subliminal_cli
+
+if TYPE_CHECKING:
+    from tests.conftest import CliRunner
+
 
 # Core test
 pytestmark = pytest.mark.core
 
 
-def test_cli_help() -> None:
-    runner = CliRunner()
-    result = runner.invoke(subliminal_cli, ['--help'])
-    assert result.exit_code == 0
-    assert result.output.startswith('Usage: subliminal [OPTIONS] COMMAND [ARGS]...')
+def test_cli_help(cli_runner: CliRunner) -> None:
+    # Run cli
+    result = cli_runner.run(subliminal_cli, ['--help'])
+
+    expected = 'Usage: subliminal [OPTIONS] COMMAND [ARGS]...'
+    assert result.out.startswith(expected)
 
 
-def test_cli_cache() -> None:
-    runner = CliRunner()
-
+def test_cli_cache(cli_runner: CliRunner) -> None:
     # Do nothing
-    result = runner.invoke(subliminal_cli, ['cache'])
+    result = cli_runner.run(subliminal_cli, ['cache'])
     assert result.exit_code == 0
-    assert result.output == 'Nothing done.\n'
+    assert result.out == 'Nothing done.\n'
 
     # Clean cache
-    result = runner.invoke(subliminal_cli, ['cache', '--clear-subliminal'])
+    result = cli_runner.run(subliminal_cli, ['cache', '--clear-subliminal'])
     assert result.exit_code == 0
-    assert result.output == "Subliminal's cache cleared.\n"
+    assert result.out == "Subliminal's cache cleared.\n"
